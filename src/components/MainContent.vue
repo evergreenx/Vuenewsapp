27d6b1104dacadd7005bfe640feb16d1ad12fa4f
evergreen2001 <dicksonevergreenido@gmail.com>
2020-10-03 01:51:38 -0700

    first commit'

---
+++ src/components/MainContent.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+   
+   <h1> Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
---
+++ src/components/SideNav.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideNav',
+  created() {
+
+  },
+  data() {
+    return {
+    }
+  },
+  props: {
+   
+  },
+  methods: {
+   
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
